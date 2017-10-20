b33c6fe3a36abe269d26fee486682727ef9f9cec
nikosomos <somos@ualberta.ca>
2017-10-19 23:34:26 -0600

    Added constructors to habit class

--- HabitTracker/app/src/main/java/com/stylepoints/habittracker/Habit.java
+++ HabitTracker/app/src/main/java/com/stylepoints/habittracker/Habit.java
@@ -1,5 +1,6 @@
 package com.stylepoints.habittracker;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -12,32 +13,78 @@ public class Habit {
     private String type;
     private String reason;
     private Date startDate;
-
-
-    // TODO: Choose which storage option we should use
-    /*  Option 1 for storing days of the week
-        Pro:    Easier to identify days inside class - Habit.saturday
-                self-explanatory(no decoding needed)
-        Con:    Lots of variables
-     */
-    private Boolean sunday;
-    private Boolean monday;
-    private Boolean tuesday;
-    private Boolean wednesday;
-    private Boolean thursday;
-    private Boolean friday;
-    private Boolean saturday;
-
-    /*  Option 2 for storing days of the week
-        Pro:    Cleaner look, only one variable needed.
-        Con:    Needs to be decoded before you can use it.
-     */
-    private Boolean[] dayOfWeek = new Boolean[7];
-
+    private WeekSchedule weekSchedule;
 
     // TODO: Decide whether to use ArrayList, or extend it to HabitList for more functionality
     private List<HabitEvent> habitEventList;
 
-    // TODO: Build constructors and getters and setters
+    public Habit(String type, WeekSchedule schedule) {
+        this.type = type;
+        this.startDate = new Date();
+        this.weekSchedule = schedule;
+        this.habitEventList = new ArrayList<HabitEvent>();
+    }
 
+    public Habit(String type, String reason, WeekSchedule schedule) {
+        this.type = type;
+        this.reason = reason;
+        this.startDate = new Date();
+        this.weekSchedule = schedule;
+        this.habitEventList = new ArrayList<HabitEvent>();
+    }
+
+    public Habit(String type, Date startDate, WeekSchedule schedule) {
+        this.type = type;
+        this.startDate = startDate;
+        this.weekSchedule = schedule;
+        this.habitEventList = new ArrayList<HabitEvent>();
+    }
+
+    public Habit(String type, String reason, Date startDate, WeekSchedule schedule) {
+        this.type = type;
+        this.reason = reason;
+        this.startDate = startDate;
+        this.weekSchedule = schedule;
+        this.habitEventList = new ArrayList<HabitEvent>();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public WeekSchedule getWeekSchedule() {
+        return weekSchedule;
+    }
+
+    public void setWeekSchedule(WeekSchedule weekSchedule) {
+        this.weekSchedule = weekSchedule;
+    }
+
+    public List<HabitEvent> getHabitEventList() {
+        return habitEventList;
+    }
+
+    public void setHabitEventList(List<HabitEvent> habitEventList) {
+        this.habitEventList = habitEventList;
+    }
 }
