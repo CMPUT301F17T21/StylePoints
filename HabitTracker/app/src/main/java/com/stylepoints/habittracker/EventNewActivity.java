27cb177bd016be07a94ad7c29c00077d112a5ff0
BradHanasyk <bradhanasyk@gmail.com>
2017-11-12 22:06:34 -0700

    Add files via upload

--- HabitTracker/app/src/main/java/com/stylepoints/habittracker/EventNewActivity.java
+++ HabitTracker/app/src/main/java/com/stylepoints/habittracker/EventNewActivity.java
@@ -5,11 +5,13 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import static android.app.Activity.RESULT_OK;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class EventNewActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,29 +19,22 @@ public class EventNewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_new);
     }
 
-    /**
-     * Called when the user taps the Send button
-     */
-    public void takePhoto(View view) { //modify for photo
-        // Do something in response to button
-        int cap = 1;
-
-        private void dispatchTakePictureIntent {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(takePictureIntent, cap);
-            }
+    public void takePhoto(int requestCode, int resultCode, Intent data, ImageView photoView) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            if (requestCode == cap && resultCode == RESULT_OK) {
-               Bundle extras = data.getExtras();
-                 imageBitmap = (Bitmap) extras.get("data");
-                mImageView.setImageBitmap(imageBitmap);
-            }
+        startActivity(takePictureIntent);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            photoView.setImageBitmap(imageBitmap);
         }
     }
+}
+
+
 
 
 
