8d966f5f50a0bbc5404320259665c8273b99e4a6
nikosomos <somos@ualberta.ca>
2017-11-22 13:51:44 -0700

    UI for User

---
+++ HabitTracker/app/src/main/java/com/stylepoints/habittracker/viewmodel/CentralHubActivity/UserNewActivity.java
@@ -0,0 +1,10 @@
+package com.stylepoints.habittracker.viewmodel.CentralHubActivity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nikosomos on 2017-11-22.
+ */
+
+public class UserNewActivity extends AppCompatActivity {
+}
