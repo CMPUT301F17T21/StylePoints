c52c451e4c9f3b68414402cba29b332f4fcc241a
nikosomos <somos@ualberta.ca>
2017-11-28 18:20:26 -0700

    Accidentally working on master, added User repository files

---
+++ HabitTracker/app/src/test/java/com/stylepoints/habittracker/UserElasticTest.java
@@ -0,0 +1,42 @@
+package com.stylepoints.habittracker;
+
+import com.stylepoints.habittracker.repository.remote.ElasticSearch;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by nikosomos on 2017-11-28.
+ */
+
+public class UserElasticTest {
+    private ElasticSearch elastic;
+
+    @Before
+    public void initElastic() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://cmput301.softwareprocess.es:8080/cmput301f17t21_stylepoints/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        elastic = retrofit.create(ElasticSearch.class);
+    }
+
+    @Test
+    public void testCreateUser(){
+
+    }
+
+    @Test
+    public void testLoadUser(){
+
+    }
+
+    @Test
+    public void testDeleteUser(){
+
+    }
+
+}
