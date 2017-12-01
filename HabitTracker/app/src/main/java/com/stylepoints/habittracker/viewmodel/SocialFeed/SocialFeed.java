a520d7846803031d8249408c63ee4a3b2946ad9b
Aman Seth <aman@Amans-MacBook-Pro.local>
2017-11-30 20:19:18 -0700

    fixed small bugs

--- HabitTracker/app/src/main/java/com/stylepoints/habittracker/viewmodel/SocialFeed/SocialFeed.java
+++ HabitTracker/app/src/main/java/com/stylepoints/habittracker/viewmodel/SocialFeed/SocialFeed.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
-
+import android.view.View;
 import com.stylepoints.habittracker.R;
 import com.stylepoints.habittracker.repository.local.AppDatabase;
 
@@ -27,5 +27,37 @@ public class SocialFeed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_feed);
+
+        bindToUi();
+
     }
+    private void bindToUi() {
+        button_mapEvents = (Button) findViewById(R.id.mapEventsButton);
+        button_followers = (Button) findViewById(R.id.followersButton);
+        button_following = (Button) findViewById(R.id.followingButton);
+
+        button_mapEvents.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SocialFeed.this, MapEvents.class);
+                startActivity(intent);
+            }
+        });
+
+        button_followers.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SocialFeed.this, Followers.class);
+                startActivity(intent);
+            }
+        });
+        button_following.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SocialFeed.this, Following.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
