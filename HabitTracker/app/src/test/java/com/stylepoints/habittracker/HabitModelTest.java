8826cf8b2759cc608fb362691f8b4ae92641fed1
nocduro <mchauck@ualberta.ca>
2017-12-02 16:02:50 -0700

    start Habit model unit tests

---
+++ HabitTracker/app/src/test/java/com/stylepoints/habittracker/HabitModelTest.java
@@ -0,0 +1,48 @@
+package com.stylepoints.habittracker;
+
+import com.stylepoints.habittracker.model.Habit;
+import com.stylepoints.habittracker.model.HabitReasonTooLongException;
+import com.stylepoints.habittracker.model.HabitTypeTooLongException;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HabitModelTest {
+
+    @Test
+    public void testReasonTooLong() {
+        String testReason = "this is a long test reason that is more than the maximum characters";
+        assertTrue(testReason.length() > Habit.MAX_REASON_LENGTH);
+        try {
+            Habit habit = new Habit("t", testReason, "username");
+            fail("Exception not thrown for a reason that is too long");
+        } catch (HabitTypeTooLongException e) {
+            fail("Type is too long for this test");
+        } catch (HabitReasonTooLongException e) {
+
+        }
+    }
+
+    @Test
+    public void testTypeTooLong() {
+        String type = "this is a long type reason that is more than the maximum characters";
+        assertTrue(type.length() > Habit.MAX_TYPE_LENGTH);
+        try {
+            Habit habit = new Habit(type, "", "username");
+            fail("Exception not thrown for a reason that is too long");
+        } catch (HabitTypeTooLongException e) {
+
+        } catch (HabitReasonTooLongException e) {
+            fail("Reason is too long for this test");
+        }
+    }
+
+    @Test
+    public void noNullInRequiredFields() throws Exception {
+        Habit habit = new Habit("testType", "testReason", "testUsername");
+        assertEquals("testType", habit.getType());
+        assertEquals("testReason", habit.getReason());
+        
+    }
+}
