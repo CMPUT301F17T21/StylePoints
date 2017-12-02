73e86d474434dc21e0b0aca935829ffdfa5ea45f
Aman Seth <aman@Amans-MacBook-Pro.local>
2017-12-02 16:43:56 -0700

    wow factor

--- HabitTracker/app/src/main/java/com/stylepoints/habittracker/viewmodel/Profile/Achievements.java
+++ HabitTracker/app/src/main/java/com/stylepoints/habittracker/viewmodel/Profile/Achievements.java
@@ -2,14 +2,28 @@ package com.stylepoints.habittracker.viewmodel.Profile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.stylepoints.habittracker.R;
 
 public class Achievements extends AppCompatActivity {
-
+    private Button question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
+
+        question = (Button) findViewById(R.id.question);
+        question.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Complete events to level up!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
+            }
+        });
     }
 }
--- HabitTracker/app/src/main/java/com/stylepoints/habittracker/viewmodel/Profile/ProfileMain.java
+++ HabitTracker/app/src/main/java/com/stylepoints/habittracker/viewmodel/Profile/ProfileMain.java
@@ -1,16 +1,43 @@
 package com.stylepoints.habittracker.viewmodel.Profile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.stylepoints.habittracker.R;
+import com.stylepoints.habittracker.viewmodel.SocialFeed.MapEvents;
+import com.stylepoints.habittracker.viewmodel.SocialFeed.SocialFeed;
 
 public class ProfileMain extends AppCompatActivity {
-
+    private Button achievements;
+    private Button question;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_main);
+        achievements = (Button) findViewById(R.id.achievements);
+        question = (Button) findViewById(R.id.question);
+        achievements.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileMain.this, Achievements.class);
+                startActivity(intent);
+            }
+        });
+        question.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Complete events to level up!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
+            }
+        });
+
     }
 }
 
